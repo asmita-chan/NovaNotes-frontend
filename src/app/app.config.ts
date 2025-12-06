275d3a237d1c142e63259ace7a633ff0a1b818ea
Asmita <dementorphantasm02@gmail.com>
2025-12-06 15:16:39 +0530

    changed layout and fontawesome stylesheet and toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { ToastrModule } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 4000,
+      progressBar: true
+    }),
+    provideHttpClient(
+      withInterceptors([jwtInterceptor])
+    )
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
-import { Notes } from './notes/notes/notes';
+import { Notes } from './pages/notes/notes';
 export const routes: Routes = [
     { path: '', component: Landing, data: { title: 'Home'}},
     { path: 'dashboard', component: Notes, data: { title: 'Notes'}},
